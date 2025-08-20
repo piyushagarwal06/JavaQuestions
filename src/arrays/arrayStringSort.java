59e522049b8801ea4180eac478d5dde07892b64e
piyushagarwal06 <peeyushagerwal720@gmail.com>
2025-08-20 16:07:02 +0530

    Array question solved

---
+++ src/arrays/arrayStringSort.java
@@ -0,0 +1,19 @@
+package arrays;
+public class arrayStringSort {
+    public static void main(String[] args) {
+        String arr[]={"zebra","Yak","ullu","monkey"};
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j].compareToIgnoreCase(arr[j+1])>0){
+                    String temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(String S:arr){
+            System.out.print(S+" ");
+        }
+    }
+}
