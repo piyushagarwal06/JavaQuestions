f78ef301f964a5a4d3cd7dc42afc068a1df38a52
piyushagarwal06 <peeyushagerwal720@gmail.com>
2025-08-27 06:39:57 +0530

    Inserting an element  At a given index

---
+++ src/arrays/InsertAnElement.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class InsertAnElement {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int element=99;
+        int index=3;
+        int grr[]=new int[arr.length+1];
+
+        for(int i=0;i<index;i++){
+            grr[i]=arr[i];
+        }
+        grr[index]=element;
+
+        for(int i=index;i<arr.length;i++){
+            grr[i+1]=arr[i];
+        }
+
+        for(int n:grr){
+            System.out.println(n);
+        }
+    }
+}
