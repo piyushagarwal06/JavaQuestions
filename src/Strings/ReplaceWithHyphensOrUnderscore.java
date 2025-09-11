e16d6b23855379ef1d4cc1cae2281ebcd8c976c1
piyushagarwal06 <peeyushagerwal720@gmail.com>
2025-09-11 19:59:07 +0530

    replace space with hyphen or underscore

---
+++ src/Strings/ReplaceWithHyphensOrUnderscore.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class ReplaceWithHyphensOrUnderscore {
+    public static void main(String[] args) {
+        String s="WHat are YOu DoiNG";
+        System.out.println(s.replace(' ','_'));
+        System.out.println(s.replace(' ','-'));
+    }
+}
