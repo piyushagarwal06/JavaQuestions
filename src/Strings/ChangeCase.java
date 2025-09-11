c288d121f48302c887a2d085ef2ed5a33aeed84e
piyushagarwal06 <peeyushagerwal720@gmail.com>
2025-09-11 19:58:16 +0530

    Change Case

---
+++ src/Strings/ChangeCase.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class ChangeCase {
+    public static void main(String[] args) {
+        String s="WHat are YOu DoiNG";
+        String p=s.toLowerCase();
+        String j=s.toUpperCase();
+        System.out.println(p);
+        System.out.println(j);
+    }
+}
