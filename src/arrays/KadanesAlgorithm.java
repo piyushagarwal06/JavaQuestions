e1dc2eb8442320bed2c41f50ce1e4336a0abaaba
piyushagarwal06 <peeyushagerwal720@gmail.com>
2025-09-16 16:48:39 +0530

    Kadane's Algorithm & alternative

--- src/arrays/KadanesAlgorithm.java
+++ src/arrays/KadanesAlgorithm.java
@@ -17,6 +17,26 @@ public class KadanesAlgorithm {
 
             return maxGlobal;
         }
+        //class Solution {//alternate approach
+        //    public int maxSubArray(int[] nums) {
+        //        int maxSum = Integer.MIN_VALUE;
+        //        int currentSum = 0;
+        //
+        //        for (int i = 0; i < nums.length; i++) {
+        //            currentSum += nums[i];
+        //
+        //            if (currentSum > maxSum) {
+        //                maxSum = currentSum;
+        //            }
+        //
+        //            if (currentSum < 0) {
+        //                currentSum = 0;
+        //            }
+        //        }
+        //
+        //        return maxSum;
+        //    }
+        //}
 
         public static void main(String[] args) {
             int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
