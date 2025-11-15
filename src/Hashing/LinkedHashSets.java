db9bc346e62007c18375430cee914610614f97c4
piyushagarwal06 <peeyushagerwal720@gmail.com>
2025-11-15 15:17:02 +0530

    Linked HashSets

---
+++ src/Hashing/LinkedHashSets.java
@@ -0,0 +1,14 @@
+package Hashing;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSets {
+    public static void main(String[] args) {
+        LinkedHashSet<String> cities=new LinkedHashSet<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Noida");
+        cities.add("Bengaluru");
+        System.out.println(cities);
+    }
+}
