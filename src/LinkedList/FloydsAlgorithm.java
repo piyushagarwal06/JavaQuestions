2872fcd61d9db26a3388bf9df32e16e193e9a72b
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-03-10 07:03:30 +0530

    Floyd's Algorithm

---
+++ src/LinkedList/FloydsAlgorithm.java
@@ -0,0 +1,41 @@
+package LinkedList;
+
+//Floyd’s Cycle Detection Algorithm is used to detect a cycle in a linked list using two pointers.
+
+public class FloydsAlgorithm {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next.next;
+
+        System.out.println("Has Cycle? " + hasCycle(head));
+    }
+}
