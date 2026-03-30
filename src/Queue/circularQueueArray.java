97a1022f102b2e5f05cc466ff90689d7107a2ac5
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-03-31 05:28:01 +0530

    circular queue

---
+++ src/Queue/circularQueueArray.java
@@ -0,0 +1,84 @@
+package Queue;
+
+public class circularQueueArray {
+    static class cqa{
+        int front=-1;
+        int rear=-1;
+        int size=0;
+        int []arr=new int[8];
+
+        void add(int val) throws Exception{
+            if(size==arr.length){
+                throw new Exception("Queue is Full!");
+            }
+            else if(size==0){
+                front=rear=0;
+                arr[0]=val;
+            }
+            else if(rear<arr.length-1){
+                arr[++rear]=val;
+            }
+            else if(rear==arr.length-1){
+                rear=0;
+                arr[0]=val;
+            }
+            size++;
+        }
+
+        int remove() throws Exception{
+            if(size==0) {
+                throw new Exception("queue is empty");
+            }
+            else{
+                int val=arr[front];
+                if(front==arr.length-1) front=0;
+
+                else front++;
+                size--;
+                return val;
+            }
+        }
+
+        int peek() throws Exception {
+            if (size == 0) {
+                throw new Exception("queue is empty");
+            } else return arr[front];
+        }
+
+        void display(){
+            if(size==0){
+                System.out.println("Queue is empty");
+                return;
+            }
+            else if(front<=rear){
+                for(int i=front;i<=rear;i++){
+                    System.out.print(arr[i]+" ");
+                }
+            }
+            else { //when rear<front
+                for (int i=front;i< arr.length;i++){
+                    System.out.print(arr[i]);
+                }
+                for(int i=0;i<=rear;i++){
+                    System.out.print(arr[i]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        cqa q=new cqa();
+        q.display();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.display();
+        q.remove();
+        q.display();
+
+
+        
+    }
+}
