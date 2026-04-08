f977b6e3f53930b1365082f636eddb3390fa85c2
piyushagarwal06 <peeyushagerwal720@gmail.com>
2026-04-08 22:52:02 +0530

    implementation of a tree

---
+++ src/Trees/implementation.java
@@ -0,0 +1,44 @@
+package Trees;
+
+public class implementation {
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val){
+            this.val=val;
+        }
+    }
+    static void display(Node root){
+        if(root==null) return;
+        System.out.print(root.val+ "->");
+        if(root.left != null)
+            System.out.print(root.left.val + ",");
+        else
+            System.out.print("null,");
+
+        if(root.right != null)
+            System.out.println(root.right.val);
+        else
+            System.out.println("null");
+        display(root.left);
+        display(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node(2);
+        Node a=new Node(4);
+        Node b=new Node(10);
+        root.left=a;
+        root.right=b;
+        Node c=new Node(6);
+        Node d=new Node(5);
+        a.left=c;
+        a.right=d;
+        Node e=new Node(11);
+        b.right=e;
+        System.out.println(root.val);
+        display(root);
+    }
+}
